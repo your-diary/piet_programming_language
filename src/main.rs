9aab0929967d5299ea0b98a0462f77a7d935c7c8
ynn <contact@your-diary.io>
2024-10-21 23:23:59 +0900

    refactor(main.rs): removes unnecessary commented codes

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,5 @@ use piet_programming_language::args::Args;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    // println!("{:?}", args);
-    // std::process::exit(0);
-
     piet_programming_language::run(&args)
 }
